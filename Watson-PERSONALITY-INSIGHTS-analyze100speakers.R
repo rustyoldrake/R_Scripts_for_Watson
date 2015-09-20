900d203fba99a079ee958d6cc1ac0324aa8cb330
Ryan Anderson <rustyoldrake@gmail.com>
2015-09-19 20:38:45 -0700

    Update Watson-PERSONALITY-INSIGHTS-analyze100speakers.R

--- Watson-PERSONALITY-INSIGHTS-analyze100speakers.R
+++ Watson-PERSONALITY-INSIGHTS-analyze100speakers.R
@@ -166,3 +166,19 @@ write.csv(alldata.cast,"post-process-PI-pivot.csv")
 # "Self-efficacy"        "Self-enhancement"     "Self-expression"      "Self-transcendence"  
 # "Stability"            "Structure"            "Sympathy"             "Trust"               
 # "Vulnerability"
+
+
+
+Copyright 2015 Ryan Anderson 
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
