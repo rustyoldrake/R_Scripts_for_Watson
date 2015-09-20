118a89b6b472394dbca7bdc0a4a2d45c0e1545b2
Ryan Anderson <rustyoldrake@gmail.com>
2015-09-19 20:37:53 -0700

    Update Watson-DIALOG-basic-R-scripts-segue-only-PUBLIC.R

--- Watson-DIALOG-basic-R-scripts-segue-only-PUBLIC.R
+++ Watson-DIALOG-basic-R-scripts-segue-only-PUBLIC.R
@@ -62,3 +62,18 @@ response  #201 = good
 content(response, "text") 
 ###  NEW: "{\"conversation_id\":13711,\"client_id\":13776,\"input\": \"delivery\",\"confidence\":0,\"response\":[\"Hi, I'm Watson! I can help you order a pizza, what size would you like?\"]}"
 ### "{\"conversation_id\":13232,\"client_id\":13336,\"input\": \"delivery\",\"confidence\":1.0,\"response\":[\"Ok, I have one Large canadian bacon pizza for delivery. Is that correct?\"]}"
+
+
+
+Copyright 2015 Ryan Anderson 
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
