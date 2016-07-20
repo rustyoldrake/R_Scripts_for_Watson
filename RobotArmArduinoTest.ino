45c3785eff60999a36b355ea4c94abd8579862ca
Ryan Anderson <rustyoldrake@gmail.com>
2016-07-20 14:46:46 -0700

    Update RobotArmArduinoTest.ino

--- RobotArmArduinoTest.ino
+++ RobotArmArduinoTest.ino
@@ -1,29 +1,25 @@
+/// COGNITIVE LEGO - ROBOT ARM - This Arduino code is meant to be LISTENING for serial commands from R
+/// 
 
+// #include <Servo.h> 
+#include <Keyboard.h>
 
+int moveServo;           // raw user input  
 
-const int ledPin1 =  1;      // the number of the LED pin
 const int ledPin2 =  2;      // the number of the LED pin
 const int ledPin3 =  3;      // the number of the LED pin
 const int ledPin4 =  4;      // the number of the LED pin
 const int ledPin5 =  5;      // the number of the LED pin
 const int ledPin6 =  6;      // the number of the LED pin
-const int ledPin7 =  7;      // the number of the LED pin
-const int ledPin8 =  8;      // the number of the LED pin
 const int ledPin13 =  13;      // the number of the LED pin
+ 
+void setup() {  
+ Serial.begin(9600);  
+ Serial.println("      Arduino Serial Servo Control");  
+ Serial.println("Press < or > to move, spacebar to center");  
+ Serial.println();  
 
-// Variables will change :
-int ledState = LOW;             // ledState used to set the LED
 
-// Generally, you should use "unsigned long" for variables that hold time
-// The value will quickly become too large for an int to store
-unsigned long previousMillis = 0;        // will store last time LED was updated
-
-// constants won't change :
-const long interval = 1000;           // interval at which to blink (milliseconds)
-
-void setup() {
-  // set the digital pin as output:
-  pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
   pinMode(ledPin4, OUTPUT);
@@ -31,29 +27,119 @@ void setup() {
   pinMode(ledPin6, OUTPUT);
   pinMode(ledPin13, OUTPUT);
 
-}
 
-void loop() {
-  // here is where you'd put code that needs to be running all the time.
+  digitalWrite(ledPin2, HIGH);
+  digitalWrite(ledPin3, HIGH);
+  digitalWrite(ledPin4, HIGH);
+  digitalWrite(ledPin5, HIGH);
+  digitalWrite(ledPin6, HIGH);
+  digitalWrite(ledPin13, HIGH);
 
-  // check to see if it's time to blink the LED; that is, if the
-  // difference between the current time and last time you blinked
-  // the LED is bigger than the interval at which you want to
-  // blink the LED.
-  unsigned long currentMillis = millis();
+}  
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+void loop() {  
+ // wait for serial input  
+ if (Serial.available() > 0) {  
+   moveServo = Serial.read();     // read the incoming byte: 
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-    
-    digitalWrite(ledPin1, ledState); // this is for the polarity of volatage - each cycle
+
+
+
+////// LEDS AND MOTORS
+
+   if (moveServo == 97) // 'a' is Claw Close
+   { 
+    digitalWrite(ledPin13, LOW); // this is for the polarity of volatage 
+    digitalWrite(ledPin2, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin2, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 122) // 'z' is Claw Open
+   { 
+    digitalWrite(ledPin13, HIGH); // this is for the polarity of volatage 
+    digitalWrite(ledPin2, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin2, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 115) // 's' is Wrist CLose
+   { 
+    digitalWrite(ledPin13, LOW); // this is for the polarity of volatage 
+    digitalWrite(ledPin3, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin3, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 120) // 'x' is Wrist Open
+   { 
+    digitalWrite(ledPin13, HIGH); // this is for the polarity of volatage 
+    digitalWrite(ledPin3, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin3, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 100) // 'd' is Elbow Close
+   { 
+    digitalWrite(ledPin13, LOW); // this is for the polarity of volatage 
+    digitalWrite(ledPin4, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin4, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 99) // 'c' is Elbow Open
+   { 
+    digitalWrite(ledPin13, HIGH); // this is for the polarity of volatage 
+    digitalWrite(ledPin4, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin4, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 102) // 'f' is SHoulder Close
+   { 
+    digitalWrite(ledPin13, LOW); // this is for the polarity of volatage 
+    digitalWrite(ledPin5, LOW);   /// 
+    delay(1000);
+    digitalWrite(ledPin5, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 118) // 'v' is Shoulder Open
+   { 
+    digitalWrite(ledPin13, HIGH); // this is for the polarity of volatage 
+    digitalWrite(ledPin5, LOW);   /// 
+    delay(1000);
+    digitalWrite(ledPin5, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 103) // 'g' is ROtate Left
+   { 
+    digitalWrite(ledPin13, LOW); // this is for the polarity of volatage 
+    digitalWrite(ledPin6, LOW);   /// CLAW
+    delay(1000);
+    digitalWrite(ledPin6, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 98) // 'b' is ROtate RIght
+   { 
+    digitalWrite(ledPin13, HIGH); // this is for the polarity of volatage 
+    digitalWrite(ledPin6, LOW);   /// 
+    delay(1000);
+    digitalWrite(ledPin6, HIGH); // RESTORE
+   } 
+
+
+   if (moveServo == 32) // ' ' is space for reset  // 
+   { 
+    digitalWrite(ledPin13, HIGH); // this is for the polarity of volatage - each cycle
 
     // OK - crude way to cycle through the relays for 2,3,4,5,6, 
     digitalWrite(ledPin2, LOW);   /// CLAW
@@ -71,7 +157,8 @@ void loop() {
     digitalWrite(ledPin6, LOW);   //
     delay(1000);
     digitalWrite(ledPin6, HIGH);   // restore before moving on
-
-  }
-}
-
+   } 
+ 
+}  
+ 
+}  
