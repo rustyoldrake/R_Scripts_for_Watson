2158cabd4561344ddcbd3e969fa31a0bf4b1fa62
Ryan Anderson <rustyoldrake@gmail.com>
2016-08-25 22:10:37 -0700

    Update keys.r
    added dummy keys for more services - check CAPS (STT) vs small (stt) in older code

--- keys.r
+++ keys.r
@@ -1,20 +1,31 @@
-# keys.R - EXAMPLE - you need to get your own from IBM BLUEMIX. SIgn up. Start service, Manage credentials. 
-# used with the WINE Demo code (STT NLC TTS demo.R)
-# YOUR PRIVATE FILE DO NOT SHARE (like I did here :)
-# http://blog.revolutionanalytics.com/2015/11/how-to-store-and-use-authentication-details-with-r.html
+# keys.R -- YOUR PRIVATE KEYS - DO NOT SHARE
+# this is not the 'pro' way to store keys, but it's low barrier to entry for many newbs  http://blog.revolutionanalytics.com/2015/11/how-to-store-and-use-authentication-details-with-r.html
+# The watson.keys.load will let you modify this if you like
 
-# STT - Speech-To-Text credentials on Bluemix
-username_stt <-"a530b62e-aaaa-406c-9cc3-XXXXXXXXX"
-password_stt <- "XXXXXXXXXXX"  
-username_password_stt = paste(username_stt,":",password_stt,sep="")
+# ALCHEMY Language - AUTHENTICATION / CREDENTIALS  - Platinum
+username_password_ALCH <- "xxxxxxxxxxaaa160dd1da4d8126c6b285"
 
-# NLC CREDENTIALS - DEMO - "NLC-COFFEE" Service on BLuemix
-username_nlc = "6c45fa90-aaaa-4d2c-afb2-XXXXXXXXXX"
-password_nlc = "XXXXXXXXXXXX"
-username_password_nlc = paste(username_nlc,":",password_nlc)
+# TONE ANALYZER  - AUTHENTICATION AND CREDENTIALS - GA
+username_TON = "xxxxxxxxx-7454-4721-b829-bcd051ef7d2d"
+password_TON = "xxxxxxxxx"
+username_password_TON = paste(username_TON,":",password_TON)
 
-# TEXT TO SPEECH - AUTHENTICATION AND CREDENTIALS 
-username_TTS <-"54816193-aaaa-486e-98de-XXXXXXXXXX" 
-password_TTS <- "XXXXXXXXXX"  
+# PERSONALITY INSIGHTS (PI) - AUTHENTICATION AND CREDENTIALS - Platinum
+username_PI = "xxxxxxxxx-e3d8-4dfb-86aa-d6ab31ee2d2c"
+password_PI = "xxxxxxxxx"
+username_password_PI = paste(username_PI,":",password_PI)
+
+# NLC CREDENTIALS - AUTHENTICATION AND CREDENTIALS - Platinum
+username_NLC = "xxxxxxxxx-e8cd-4dbf-816b-6a7ac1f2dd71"
+password_NLC = "xxxxxxxxx"
+username_password_NLC = paste(username_NLC,":",password_NLC,sep="")
+
+# STT - Speech-To-Text (STT) - RED -  AUTHENTICATION AND CREDENTIALS
+username_STT <-"xxxxxxxxx-b5bb-406c-9cc3-7cb964fb614f"
+password_STT <- "xxxxxxxxx"
+username_password_STT = paste(username_STT,":",password_STT,sep="")
+
+# TTS - TEXT TO SPEECH (TTS) - RED - AUTHENTICATION AND CREDENTIALS
+username_TTS <-"xxxxxxxxx-48c4-486e-98de-174388b22b17"
+password_TTS <- "xxxxxxxxx"
 username_password_TTS = paste(username_TTS,":",password_TTS,sep="")
-
